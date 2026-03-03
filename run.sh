304fb4122b182a5ab982731ca717db3b5346cfa9
sketchloop <wokman69@tutamail.com>
2026-03-03 14:47:55 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Start the Spigot server
 cd server
-java -Xmx1G -jar server.jar nogui
+java -Xmx1G -jar server.jar nogui &
+cd ..
+
+# Start the EaglercraftX proxy
+cd proxy
+java -Xmx512M -jar proxy.jar
